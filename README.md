46bdb1257ee291f8e7c09fc74ee914034a816f43
mustaphasadiq0015 <124324511+mustaphasadiq0015@users.noreply.github.com>
2025-04-18 22:47:45 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+agrihub api
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import xgboost as xgb
+import joblib
+
+app = Flask(__name__)
+
+model = xgb.XGBRegressor()
+model.load_model("commodity_price_model.json")
+label_encoders = joblib.load("label_encoders.pkl")
+
+features = ["admin1", "admin2", "market", "category", "commodity",
+            "unit", "pricetype", "currency", "year", "month", "day"]
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame([data])
+
+    for col in label_encoders:
+        if col in df.columns:
+            le = label_encoders[col]
+            df[col] = df[col].map(lambda x: le.transform([x])[0] if x in le.classes_ else -1)
+
+    prediction = model.predict(df[features])[0]
+    return jsonify({'predicted_price': round(float(prediction), 2)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+  
